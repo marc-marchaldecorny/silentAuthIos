87580b7bd6250a79abbb8cfa4f20f9fa012d7524
marcdecorny <marcdecorny@gmail.com>
2023-10-19 14:17:24 +0200

    Initial Commit

---
+++ SilentAuthIos/SilentAuthIosApp.swift
@@ -0,0 +1,17 @@
+//
+//  SilentAuthIosApp.swift
+//  SilentAuthIos
+//
+//  Created by Marc Marchal de Corny on 19/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct SilentAuthIosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
